fad82547327691cfd56f424a79badfcf6780506f
VictorLira-DEV <victorliradev@gmail.com>
2021-12-29 20:47:53 -0200

    Helpers and Configuration Files

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,14 @@
+import {async} from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers'
+
 export const state = {
     recipe: {},
 };
 
 export const loadRecipe = async function (id) {
     try {
-        const res = await fetch(
-            // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-        );
-
-        const data = await res.json();
-        if (!res.ok) throw new Error(`${data.message} (${data.status}) `);
+        const data = await getJSON(`${API_URL}/${id}`)
 
         const { recipe } = data.data;
 
@@ -25,6 +23,6 @@ export const loadRecipe = async function (id) {
             ingredients: recipe.ingredients,
         };
     } catch (err) {
-        console.log(err);
+        console.log(err + "* * * *");
     }
 };
