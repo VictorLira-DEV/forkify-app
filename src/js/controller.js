fba962219d441471f619b3c86f615515777bfe11
VictorLira-DEV <victorliradev@gmail.com>
2022-01-03 18:25:23 -0200

    Implementing Pagination - Part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
+import paginationView from './views/paginationView';
 import '../sass/main.scss';
 // with this we make sure that most real old browser are being supported by our app
 import 'core-js/stable';
@@ -26,20 +27,31 @@ const controlRecipe = async function () {
 const controlSearchResult = async function () {
     try {
         resultsView.renderSpinner();
-        console.log(resultsView);
         const query = searchView.getQuery();
+        
         if (!query) return;
 
         await model.loadSearchResults(query);
-        resultsView.render(model.getSearchResultsPage());
+        resultsView.render(model.getSearchResultsPage(3));
+
+        //initial pagination buttons
+        paginationView.render(model.state.search);
     } catch (err) {
         console.log(err);
     }
+};
+
+const controlPagination = function (goToPage) {
+    resultsView.render(model.getSearchResultsPage(goToPage));
+
+    //initial pagination buttons
+    paginationView.render(model.state.search);
 };
 
 // Event Handlers in MVC: Publisher-Subscriber Pattern
 const init = function () {
     recipeView.addHandlerRender(controlRecipe);
     searchView.addHandlerSearch(controlSearchResult);
+    paginationView.addHandlerClick(controlPagination);
 };
 init();
